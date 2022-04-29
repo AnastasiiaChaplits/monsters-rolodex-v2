9c845d4012f7d7a4c6fc7b31d7de7deaacbf3c3e
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-04-29 13:57:49 +0200

    implement app using class components

---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+import "./styles.css";
+import Card from "../Card/Card";
+
+class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => (
+          <Card monster={monster} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CardList;
